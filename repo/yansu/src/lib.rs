c18a28a7c211ea9fcadab931ab2181cfa70c78ec
sho-chan-081 <showsay0908@gmail.com>
2025-06-19 20:16:13 +0900

    20250619

--- repo/yansu/src/lib.rs
+++ repo/yansu/src/lib.rs
@@ -4,9 +4,11 @@
 #![test_runner(crate::test_runner::test_runner)]
 #![reexport_test_harness_main = "run_unit_tests"]
 #![no_main]
+pub mod allocator;
 pub mod graphics;
 pub mod qemu;
 pub mod result;
+pub mod serial;
 pub mod uefi;
 pub mod x86;
 
@@ -15,6 +17,9 @@ pub mod test_runner;
 
 #[cfg(test)]
 #[no_mangle]
-pub fn efi_main() {
+fn efi_main(image_handle: uefi::EfiHandle, efi_system_table: &uefi::EfiSystemTable) {
+    let mut memory_map = uefi::MemoryMapHolder::new();
+    uefi::exit_from_efi_boot_services(image_handle, efi_system_table, &mut memory_map);
+    allocator::ALLOCATOR.init_with_mmap(&memory_map);
     run_unit_tests()
 }
