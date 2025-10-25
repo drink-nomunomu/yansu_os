5f66f8520e963086e96b761f19666364f5c59bde
garireo2549 <freedomgreo@gmail.com>
2025-10-25 10:39:18 +0900

    ganpura

--- repo/yansu/src/xhci.rs
+++ repo/yansu/src/xhci.rs
@@ -1,5 +1,4 @@
 extern crate alloc;
-
 use crate::allocator::ALLOCATOR;
 use crate::bits::extract_bits;
 use crate::executor::spawn_global;
@@ -18,6 +17,7 @@ use crate::slice::Sliceable;
 use crate::volatile::Volatile;
 use crate::x86::busy_loop_hint;
 use alloc::boxed::Box;
+use alloc::collections::BTreeSet;
 use alloc::collections::VecDeque;
 use alloc::rc::Rc;
 use alloc::rc::Weak;
@@ -243,9 +243,22 @@ impl PciXhciDriver {
                     UsbHidProtocol::BootProtocol as u8,
                 )
                 .await?;
+                let mut prev_pressed = BTreeSet::new();
                 loop {
-                    let report = Self::request_hid_report(&xhc, slot, &mut ctrl_ep_ring).await?;
-                    info!("xhci: hid report: {report:?}");
+                    let pressed = {
+                        let report =
+                            Self::request_hid_report(&xhc, slot, &mut ctrl_ep_ring).await?;
+                        BTreeSet::from_iter(report.into_iter().skip(2).filter(|id| *id != 0))
+                    };
+                    let diff = pressed.symmetric_difference(&prev_pressed);
+                    for id in diff {
+                        if pressed.contains(id) {
+                            info!("usb_keyboard: key down: {id}");
+                        } else {
+                            info!("usb_keyboard: key up : {id}");
+                        }
+                    }
+                    prev_pressed = pressed;
                 }
             }
         }
